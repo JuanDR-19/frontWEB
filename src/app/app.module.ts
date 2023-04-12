294664435f08c23d9adc2f9fb26653176210646a
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-04-12 18:21:26 -0500

    corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { PaginationButtonsComponent } from './pagination-buttons/pagination-buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavmenuComponent,
+    MainMenuComponent,
+    PaginationButtonsComponent
   ],
   imports: [
     BrowserModule,
