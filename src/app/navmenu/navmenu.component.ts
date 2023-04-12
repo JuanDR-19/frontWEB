831be8354544784bbce8bd14d6800d575d8cc104
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-04-12 18:23:13 -0500

    changes

---
+++ src/app/navmenu/navmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navmenu',
+  templateUrl: './navmenu.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class NavmenuComponent {
+
+}
---
+++ src/app/pagination-buttons/pagination-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pagination-buttons',
+  templateUrl: './pagination-buttons.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class PaginationButtonsComponent {
+
+}
