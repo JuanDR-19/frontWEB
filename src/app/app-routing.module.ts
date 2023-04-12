b9517c64aeb3686e91127bafcc3e7987b228fbd5
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-04-12 18:41:53 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent}
