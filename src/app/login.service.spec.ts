cadacebf1e02f2be6cffcb88b29e5e69cefe9b6c
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2023-04-26 18:49:11 -0500

    Update login.service.spec.ts

--- src/app/login.service.spec.ts
+++ src/app/login.service.spec.ts
@@ -4,6 +4,7 @@ import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let token='BearerABC1234';
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +13,10 @@ describe('LoginService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('El token puede ser obtenido de las cookies',()=>{
+    service.setToken(token);
+    expect(service.getToken).toBe(token);
   });
 });
