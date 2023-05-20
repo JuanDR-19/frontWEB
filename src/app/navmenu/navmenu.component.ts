1db7372beb46dc9a1178db1fbe4d316d6fa73d8c
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-05-20 12:02:56 -0500

    .

--- src/app/navmenu/navmenu.component.ts
+++ src/app/navmenu/navmenu.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ToolService } from "../tool.service";
-import { CookieService  } from "ngx-cookie-service";
-
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: 'navmenu',
@@ -9,64 +8,50 @@ import { CookieService  } from "ngx-cookie-service";
   styleUrls: ['../app.component.css']
 })
 
-/**
-
- @description Compononete que contiene el header de navegacion.
- @class NavmenuComponent
- @constructor
- @param {ToolService} toolservice -Servicio que se encarga de realizar las peticiones http a microservicio necesario
- */
 export class NavmenuComponent {
 
-  constructor(private toolservice: ToolService,private cookies:CookieService) {}
+  constructor(private toolservice: ToolService, private cookies: CookieService) {}
 
-  /**
-   * nombre de marca o nombre de herramienta a buscar
-   * @type {String} word
-   */
-  word="";
+  word = "";
+  searchType = "marca";
   Authenticated = 0;
 
-  ngOnInit(){
+  ngOnInit() {
     this.checkAuthentication();
   }
 
-  ngOnChanges(){
+  ngOnChanges() {
     this.checkAuthentication();
   }
 
-  /**
+  search() {
+    if (this.searchType === "brand") {
+      this.SearchBrand();
+    } else if (this.searchType === "name") {
+      this.SearchName();
+    }
+  }
 
-   @description Método encargado de llamar al servicio para buscar herramientas por marca
-   @method SearchBrand
-   */
   SearchBrand() {
-
     this.toolservice.searchBrand(this.word).subscribe(
       (data) => {
         this.toolservice.updateTools(data);
       }
-    )
+    );
   }
 
-  /**
-
-   @description Método que se ejecuta al hacer clic en el botón "Buscar por nombre" para llamar al servicio que busca las herramientas por nombre
-   @method SearchName
-   */
   SearchName() {
-
     this.toolservice.searchName(this.word).subscribe(
       (data) => {
         this.toolservice.updateTools(data);
       }
-    )
+    );
   }
 
-  logOut(){
+  logOut() {
     this.cookies.delete('token');
     this.cookies.delete("user");
-    self.location.reload();
+    location.reload();
   }
 
   private checkAuthentication() {
@@ -74,5 +59,4 @@ export class NavmenuComponent {
       this.Authenticated = 1;
     }
   }
-
 }
